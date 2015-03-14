31d41baa78eec3556b1be5de40627bf0081b1e54
Simon Harrison <hypertexture@gmail.com>
2015-03-14 14:10:16 +0000

    Minor Changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,82 @@
-module.exports = function(grunt) {
-
-    // 1. All configuration goes here
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-
-        sass: {
-            dev: {
-                options: {
-                    loadPath: [
-                        'scss'
-                    ],
-                    style: 'expanded',
-                    sourcemap: 'none',
-                    noCache: true
-                },
-                files: {
-                    'css/main.css': 'scss/main.scss'
-                }
-            }
-        },
-
-        watch: {
-            sass: {
-                files: [
-                    'scss/*.scss'
-                ],
-                tasks: ['sass:dev'],
-                options: {
-                    spawn: false,
-                    livereload: true
-                }
-            }
-
-        }
-
-    });
-
-
-
-    // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-
-
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['watch']); // 'ctrl and c' to stop
-    grunt.registerTask('css-dev', ['sass:dev']);
-
-
-
-};
+module.exports = function(grunt) {
+
+    // because why not
+    "use strict";
+
+    // 1. All configuration goes here
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+
+        devUpdate: {
+            main: {
+                options: {
+                    reportUpdated: false,
+                    updateType: "prompt",
+                    semver: false
+                }
+            }
+        },
+
+        concat: {
+            dist: {
+                src: [
+                    'js/plugins.js', // This specific file
+                    'js/scripts.js'  // This specific file
+                ],
+                dest: 'js/production.js',
+            }
+        },
+
+        jshint: {
+            beforeconcat: ['js/scripts.js'],
+        },
+
+        uglify: {
+            global: {
+                files: {
+                    "js/production.min.js": "js/production.js"
+                }
+            }
+        },
+
+        sass: {
+            global: {
+                options: {
+                    style: "compressed",
+                    precision: 10
+                },
+                files: {
+                    "css/main-unprefixed.css": "scss/main.scss"   
+                }
+            }
+        },
+
+        autoprefixer: {
+            global: {
+                src: "css/main-unprefixed.css",
+                dest: "css/main.css"
+            }
+        },
+
+        watch: {
+            options: {
+                spawn: false
+            },
+            js: {
+                files: ["js/*.js"],
+                tasks: ["jshint", "concat", "uglify"]
+            },
+            css: {
+                files: ["scss/**/*.scss"],
+                tasks: ["sass", "autoprefixer"]
+            }
+        }
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    require("load-grunt-tasks")(grunt);
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask("default", ["sass", "autoprefixer", "watch"]);
+
+};
